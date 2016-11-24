12cb5f393290f5dc873a64f2d59abc44edd7b84f
Melka Martin <melka@avast.com>
2016-11-24 18:20:18 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Currently only available for Google Chrome."""
 
 setup(
     name='webstore_deployer',
-    version='0.2',
+    version='0.3',
     description='Publish browser extensions to their stores.',
     long_description=long_description,
     author='Martin Melka',
