53756bf5daa75160ab1d240752ed5649cfbcae15
Melka Martin <melka@avast.com>
2016-11-24 13:13:18 +0100

    Add readme and setup.py

---
+++ README.md
@@ -0,0 +1,10 @@
+## Installation
+To install `webstore-deployer` from Avast Artifactory, run the following command:
+
+```python -m pip install --extra-index-url https://artifactory.srv.int.avast.com/artifactory/api/pypi/pypi-local/simple webstore-deployer```
+
+## Usage
+General usage to get Chrome extension uploaded is:
+* Run ```webstore-deploy init <CLIENT_ID>``` and follow instructions on screen, then
+* Run ```webstore-deploy auth <CLIENT_ID> <CLIENT_SECRET> <CODE>``` with `<CODE>` returned from the previous step. Save the `refresh_token` returned.
+* Run ```webstore-deploy upload [--filetype=crx|zip] <CLIENT_ID> <CLIENT_SECRET> <REFRESH_TOKEN> <APP_ID> <FILENAME>```
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+long_description = """Publish browser extensions to their stores.
+Currently only available for Google Chrome."""
+
+setup(
+    name='webstore_deployer',
+    version='0.2',
+    description='Publish browser extensions to their stores.',
+    long_description=long_description,
+    author='Martin Melka',
+    author_email='melka@avast.com',
+    license='MIT',
+    keywords='extension, browser, chrome, store',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development'
+    ],
+    url='<not available yet>',
+    include_package_data=True,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'webstore-deploy = webstore_deployer.webstore:main'
+        ]
+    },
+    install_requires=['click>=6', 'requests', ]
+)
