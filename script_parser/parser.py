b51e02b525ec5a406d9d9f2fb8bc0cee8d19f5f1
Melka Martin <melka@avast.com>
2017-01-18 13:19:26 +0100

    Add parser stub

---
+++ script_parser/parser.py
@@ -0,0 +1,77 @@
+import re
+import os
+
+
+class Functions:
+    def chrome_new(self):
+        print("chrome_new")
+
+
+class Parser:
+    functions = {
+        'chrome.new': Functions.chrome_new
+    }
+
+    def __init__(self, script=None, script_fn=None):
+        super().__init__()
+        if (not script and not script_fn) or (script and script_fn):
+            raise ValueError("One of script or script_fn must be set!")
+
+        if script_fn:
+            self.script = open(script_fn).readlines()
+        else:
+            self.script = script
+
+        self.variables = {}
+
+    def execute(self):
+        for line in self.script:
+            self.execute_line(line)
+
+    def execute_line(self, line: str):
+        tokens = line.strip().split(" ")
+        tokens = self.resolve_variables(tokens)
+
+        # actually execute
+
+    def token_to_func(self, token):
+        try:
+            return self.functions[token]
+        except KeyError:
+            raise FunctionNotDefinedException(token)
+
+    def resolve_variables(self, tokens):
+        res = []
+        for token in tokens:
+            res.append(self.resolve_variable(token))
+
+        return res
+
+    def resolve_variable(self, token: str):
+        match = re.match(r'\s*\$\{([^}]+)\}\s*', token)
+        if match:
+            var = match.group(1)
+            if var.startswith('env.'):
+                # environment variable
+                var = var.split('.', maxsplit=1)[1]
+                value = os.getenv(var)
+            else:
+                # normal variable - read from self
+                try:
+                    value = self.variables[var]
+                except KeyError:
+                    raise VariableNotDefinedException(var)
+
+            print("{} -> {}".format(var, value))
+            return value
+        else:  # token is not a variable, just return it
+            print("{} is not a variable token".format(token))
+            return token
+
+
+class VariableNotDefinedException(Exception):
+    pass
+
+
+class FunctionNotDefinedException(Exception):
+    pass
---
+++ tests/script_parser/test_variable_expansion.py
@@ -0,0 +1,28 @@
+from script_parser import parser
+import pytest
+
+import os
+
+
+def test_resolve_variable():
+    p = parser.Parser(['foo'])
+    p.variables['abc'] = 'hello'
+    assert p.resolve_variable("${abc}") == 'hello'
+
+
+def test_resolve_variable_unset():
+    p = parser.Parser(['foo'])
+    with pytest.raises(parser.VariableNotDefinedException):
+        p.resolve_variable("${xyz}") == 'hello'
+
+
+def test_resolve_env_variable():
+    os.environ['abc'] = 'hello'
+    p = parser.Parser(['foo'])
+    assert p.resolve_variable("${env.abc}") == 'hello'
+
+
+def test_resolve_env_variable_unset():
+    os.environ['xyz'] = ''
+    p = parser.Parser(['foo'])
+    assert not p.resolve_variable("${env.xyz}")
