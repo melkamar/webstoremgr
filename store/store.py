935457f20c8c98b0dcad8d512e25cf7bc5ab14d1
Martin Melka <melkamar@fit.cvut.cz>
2017-01-30 18:22:47 +0100

    Add Store class doc

--- store/store.py
+++ store/store.py
@@ -2,6 +2,17 @@ import requests
 
 
 class Store:
+    """
+    Base class representing any webstore.
+
+    """
+
     def __init__(self, session=None):
+        """
+
+        Args:
+            session(requests.Session): If supplied, this session will be used for all internet communication.
+             If none, a new session if created.
+        """
         super().__init__()
         self.session = session or requests.Session()
